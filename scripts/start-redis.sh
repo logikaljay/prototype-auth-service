db256ae35470a7e2d3b4ae70070b2d9d0d25a6d3
Jay Baker <logikal@gmail.com>
2015-12-28 13:19:43 +1300

    fix(Build): changing redis port

--- scripts/start-redis.sh
+++ scripts/start-redis.sh
@@ -1,4 +1,4 @@
 NAME="prototype-auth-redis"
 PORT=6379
 
-docker run --name ${NAME} -p ${PORT}:${PORT} -d redis redis-server --appendonly yes
+docker run --name ${NAME} -p 9736:${PORT} -d redis redis-server --appendonly yes
--- src/lib/cache.js
+++ src/lib/cache.js
@@ -8,7 +8,7 @@ class Cache {
     constructor(opts) {
         var defaults = {
             host: '192.168.99.100',
-            port: 6379,
+            port: 9736,
             socket_keepalive: false
         }
 
